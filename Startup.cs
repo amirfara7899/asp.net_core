54fb8163c331c20e09628749938d39bbc09a8043
amir <69910242+amirfara7899@users.noreply.github.com>
2020-08-22 00:24:08 +0430

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+using DigikalaManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DigikalaManagement
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(
+                options => options.UseSqlServer(_configuration.GetConnectionString("MvcDigiContext")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();//or use service.AddMvc() that is in razor pages
+            services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ ViewModels/HomeDetalilsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DigikalaManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DigikalaManagement.ViewModels
+{
+    public class HomeDetalilsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
